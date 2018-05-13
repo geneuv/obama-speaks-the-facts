0dd28730a2a026fbce323ee0eb73f0d5550f4d74
Gene Uvarov <geneuvarov@live.com>
2018-05-13 14:40:34 -0700

    Updated home route

--- main.py
+++ main.py
@@ -19,10 +19,17 @@ def get_fact():
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact()
+    #fact = "hello how are you"
+    response = requests.post(url = "http://talkobamato.me/synthesize.py",
+                            data = {"input_text": fact},
+                            allow_redirects = False)
+
+    return """<h2>Click below link</h2>
+            <a href={}>Click this link</a>""".format(response.headers["Location"])
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
 
